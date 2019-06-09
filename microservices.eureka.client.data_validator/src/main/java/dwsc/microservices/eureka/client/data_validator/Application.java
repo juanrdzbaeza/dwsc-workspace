10da84d5cb67d589e0ed08a5dfef4745934892f3
juanrdzbaeza <juanrdzbaeza@gmail.com>
2019-06-09 19:54:20 +0200

    007. Inicio y preparo el microservicio para el validator, maven update.

---
+++ microservices.eureka.client.data_validator/src/main/java/dwsc/microservices/eureka/client/data_validator/Application.java
@@ -0,0 +1,13 @@
+package dwsc.microservices.eureka.client.data_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
