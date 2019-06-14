02db31c281e6150c030389d1b48747d14a2e035b
juanrdzbaeza <juanrdzbaeza@gmail.com>
2019-06-14 22:30:52 +0200

    024. Subida masiva final

--- microservice.eureka.client.manageplayer/src/main/java/dwsc/microservice/eureka/client/manageplayer/feign/clients/DataValidatorClient.java
+++ microservice.eureka.client.manageplayer/src/main/java/dwsc/microservice/eureka/client/manageplayer/feign/clients/DataValidatorClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("SAMPLE-CLIENT-VALIDATOR")
+@FeignClient("CLIENT-DATA-VALIDATOR")
 public interface DataValidatorClient {
 	@RequestMapping(value = "/{data}", method = RequestMethod.GET)
 	public ResponseEntity<Boolean> validateData(@PathVariable("data") String data); 
